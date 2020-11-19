1a3b5a5fa40451d00fbb08b457e3eb9d51cc6009
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-19 16:12:11 -0300

    Change name of entry point

--- PokemonAPI/src/app.js
+++ PokemonAPI/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
-
 const app = express()
+
+const routesPkmn = require('./routes/pokemon')
+
+app.use('/', routesPkmn)
 
 app.listen(3000, ()=> console.log('Corriendo en el puerto 3000'))
